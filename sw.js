2a60abdd66784cb783613a261ed17c51e0bd4a00
bsgamerz0507-cmyk <bsgamerz0507@gmail.com>
2026-08-30 13:01:18 +0530

    Added upload dates and PWA app support

--- sw.js
+++ sw.js
@@ -1,32 +1,21 @@
-const CACHE_NAME = "bs-gamer-z-v1";
-
-const FILES_TO_CACHE = [
-    "./",
-    "./index.html",
-    "./contact.html",
-    "./manifest.json",
-    "./css/style.css",
-    "./js/app.js",
-    "./js/settings.js",
-    "./images/logo.jpeg"
+const CACHE_NAME = 'bs-gamerz-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './css/style.css',
+  './manifest.json'
 ];
 
-self.addEventListener("install", event => {
-
-    event.waitUntil(
-
-        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-
-    );
-
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("fetch", event => {
-
-    event.respondWith(
-
-        caches.match(event.request).then(response => response || fetch(event.request))
-
-    );
-
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });

a3065996bdb3c9e5076aedfb4b8ca88586713e13
bsgamerz0507-cmyk <bsgamerz0507@gmail.com>
2026-09-05 12:00:46 +0530

    Fixed service worker cache issue

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bs-gamerz-v2';
+const CACHE_NAME = 'bs-gamerz-v3';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
@@ -19,16 +19,21 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-  // Network-first strategy (Always try to get the newest version, fall back to cache if offline)
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, copy);
-        });
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+  // If it's a video or the JSON data, don't cache, just fetch fresh!
+  if (event.request.url.includes('youtube.json') || event.request.url.includes('.jpg')) {
+    event.respondWith(fetch(event.request));
+  } else {
+    // If it's the main page or CSS, try network first, then cache
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, copy);
+          });
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+  }
 });
